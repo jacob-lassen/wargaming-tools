1ea691ae1b67ccb6d794c03147c6fffd2372c33f
jacob lassen <jabberla5@gmail.com>
2026-02-21 16:40:31 +0100

    use resolve

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import { resolve } from '$app/paths';
 	import { page } from '$app/state';
 	import { Navbar, NavBrand, NavHamburger, NavLi, NavUl } from 'flowbite-svelte';
 
@@ -9,10 +10,10 @@
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 <Navbar class="border-b bg-slate-950 px-4 py-2.5">
-	<NavBrand href="/" class="text-xl font-semibold text-slate-200">Wargame Tools</NavBrand>
+	<NavBrand href={resolve('/')} class="text-xl font-semibold text-slate-200">Wargame Tools</NavBrand>
 	<NavHamburger />
 	<NavUl activeUrl={page.url.pathname}>
-		<NavLi href="/info-cards" class="text-slate-200">Info cards</NavLi>
+		<NavLi href={resolve('/info-cards')} class="text-slate-200">Info cards</NavLi>
 	</NavUl>
 </Navbar>
 
